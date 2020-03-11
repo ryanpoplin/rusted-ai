7bfea888db060da8890ed7f0c104c2805d64b386
ryanpoplin <byrdannfox@gmail.com>
2020-03-11 02:23:46 -0400

    adding cargo setup

---
+++ rusted-ai/target/debug/deps/rusted_ai-ac3356f099f8c49f.d
@@ -0,0 +1,5 @@
+/Users/ryanpoplin/rusted-ai/rusted-ai/target/debug/deps/rusted_ai-ac3356f099f8c49f: src/main.rs
+
+/Users/ryanpoplin/rusted-ai/rusted-ai/target/debug/deps/rusted_ai-ac3356f099f8c49f.d: src/main.rs
+
+src/main.rs:
---
+++ rusted-ai/target/debug/rusted-ai.d
@@ -0,0 +1,1 @@
+/Users/ryanpoplin/rusted-ai/rusted-ai/target/debug/rusted-ai.dSYM: /Users/ryanpoplin/rusted-ai/rusted-ai/src/main.rs
